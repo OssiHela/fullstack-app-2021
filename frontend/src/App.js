7adbdc556757e98b86399d4217347cfd08f1a4f3
OssiHela <ossi.hela@gmail.com>
2022-01-11 20:27:19 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Column from "./components/column";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: [],
+      loading: false,
+    };
+    this.getWordsRequest = this.getWordsRequest.bind(this);
+  }
+
+  componentDidMount() {
+    this.getWordsRequest();
+  }
+
+  async getWordsRequest() {
+    const response = await fetch("http://localhost:8080/words");
+    const data = await response.json();
+    this.setState({ words: data }, () => {
+      console.log(this.state.words.map((word) => word.word));
+    });
+  }
+
+  render() {
+    return (
+      <div className="content">
+        <Column words={this.state.words} />
+      </div>
+    );
+  }
 }
-
-export default App;
