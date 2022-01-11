2d71d8d51c39d47f6419931dd3da30cb9b82970b
OssiHela <ossi.hela@gmail.com>
2022-01-11 19:46:04 +0200

    Update words.js
    Add express router

---
+++ routes/words.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const connection = require("../database/crudrepository.js");
+var router = express.Router();
+
+router.use(express.json());
+
+// Returns all the words from the database
+router.get("/", async (req, res) => {
+  try {
+    let result = await connection.findAll();
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// Returns a word with a certain id from the database
+router.get("/:index([0-9]+)", async (req, res) => {
+  try {
+    let result = await connection.findById(req.params.index);
+    res.send(result);
+  } catch (err) {
+    res.statusCode = 404;
+    res.send(err);
+  }
+});
+
+// Deletes a word with a certain id from the database
+router.delete("/:index([0-9]+)", async (req, res) => {
+  try {
+    let result = await connection.deleteById(req.params.index);
+    res.send(result);
+  } catch (err) {
+    res.statusCode = 404;
+    res.send(err);
+  }
+});
+
+// Adds a word to the database
+router.post("/", async (req, res) => {
+  try {
+    let result = await connection.save(req.body);
+    res.send(result);
+  } catch (err) {
+    res.statusCode = 400;
+    res.send(err);
+  }
+});
+
+// Closes the database connection
+exports.closeDB = async function () {
+  let result = await connection.close();
+  console.log(result);
+};
+
+exports.wordRouter = router;
