fad5d6312768bf4ba23187ed1f1c29f1f000a731
OssiHela <ossi.hela@gmail.com>
2022-01-12 20:36:20 +0200

    Update App.js
    Add language change
    Add score

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,37 @@
 import Column from "./components/column";
 import { Component } from "react";
+import Button from "@mui/material/Button";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       words: [],
+      language: "eng",
+      score: 0,
       loading: false,
     };
     this.getWordsRequest = this.getWordsRequest.bind(this);
+    this.changeLanguage = this.changeLanguage.bind(this);
+    this.addScore = this.addScore.bind(this);
   }
 
   componentDidMount() {
     this.getWordsRequest();
+  }
+
+  changeLanguage() {
+    if (this.state.language === "eng") {
+      this.setState({ language: "fin" });
+    } else {
+      this.setState({ language: "eng" });
+    }
+  }
+
+  addScore(amount) {
+    this.setState((state) => ({
+      score: state.score + amount,
+    }));
   }
 
   async getWordsRequest() {
@@ -26,7 +45,20 @@ export default class App extends Component {
   render() {
     return (
       <div className="content">
-        <Column words={this.state.words} />
+        <Button
+          onClick={() => {
+            this.changeLanguage();
+          }}
+        >
+          {this.state.language === "eng" ? "Vaihda kieli" : "Change language"}
+        </Button>
+        <Column
+          words={this.state.words}
+          score={this.state.score}
+          language={this.state.language}
+          changeLanguage={this.changeLanguage}
+          addScore={this.addScore}
+        />
       </div>
     );
   }
